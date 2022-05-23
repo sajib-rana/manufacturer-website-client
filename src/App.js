633093589d15cc5127b1b004227de5b342e8bec1
sajib-rana <sajibrana444@gmail.com>
2022-05-24 01:43:01 +0600

    banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
+import Portfolio from './Pages/Portfolio/Portfolio';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/portfolio' element={<Portfolio></Portfolio>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from './Banner';
+import BusinessSummery from './BusinessSummery';
+import Reviews from './Reviews';
 
 const Home = () => {
     return (
         <div>
-            
+            <Banner></Banner>
+            <BusinessSummery></BusinessSummery>
+            <Reviews></Reviews>
         </div>
     );
 };
